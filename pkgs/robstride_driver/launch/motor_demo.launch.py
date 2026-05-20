ba436d1fa409a9663f05f379569cd53ede252c3a
liaotian <liaotian724@gmail.com>
2026-05-20 18:04:51 +0800

    New launch

--- pkgs/robstride_driver/launch/motor_demo.launch.py
+++ pkgs/robstride_driver/launch/motor_demo.launch.py
@@ -17,23 +17,73 @@
 
 
 
+# from launch import LaunchDescription
+# from launch_ros.actions import Node
+
+# def generate_launch_description():
+#     motor_node = Node(
+#         package="rs_motor_ros2",
+#         executable="motor_control_node",
+#         name="motor_control_node",
+#         output="screen",
+
+#         # ======================
+#         # 👉 在这里改电机ID！！！
+#         # ======================
+#         parameters=[
+#             {"can_interface": "can0"},
+#             {"motor_ids": [1, 2]}
+#         ]
+#     )
+
+#     return LaunchDescription([motor_node])
+
+
+
+# from launch import LaunchDescription
+# from launch_ros.actions import Node
+
+# def generate_launch_description():
+#     return LaunchDescription([
+
+#         # 电机 1 节点
+#         Node(
+#             package="rs_motor_ros2",
+#             executable="motor_control_node",
+#             name="motor1_node",
+#             parameters=[{"motor_id": 1}]  # 电机1 ID
+#         ),
+
+#         # 电机 2 节点
+#         Node(
+#             package="rs_motor_ros2",
+#             executable="motor_control_node",
+#             name="motor2_node",
+#             parameters=[{"motor_id": 2}]  # 电机2 ID
+#         ),
+#     ])
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    motor_node = Node(
-        package="rs_motor_ros2",
-        executable="motor_control_node",
-        name="motor_control_node",
-        output="screen",
+    return LaunchDescription([
 
-        # ======================
-        # 👉 在这里改电机ID！！！
-        # ======================
-        parameters=[
-            {"can_interface": "can0"},
-            {"motor_ids": [1, 2]}
-        ]
-    )
+        # 🚀 节点 1：负责控制 ID 为 1 的电机
+        Node(
+            package="rs_motor_ros2",
+            executable="motor_control_node",
+            name="motor1_node",          # ROS2 节点名
+            output="screen",
+            parameters=[{"motor_id": 1}] # 传给 C++ 的单个 ID 参数
+        ),
 
-    return LaunchDescription([motor_node])
+        # 🚀 节点 2：负责控制 ID 为 2 的电机
+        Node(
+            package="rs_motor_ros2",
+            executable="motor_control_node",
+            name="motor2_node",          # ROS2 节点名
+            output="screen",
+            parameters=[{"motor_id": 2}] # 传给 C++ 的单个 ID 参数
+        ),
+    ])
